04bdd9b248c49764d1eecc72743fd00c0d6dfea6
Z0O0M <cody.volz@hotmail.com>
2019-01-30 19:23:36 +0100

    Header and footer added + materialize framework

---
+++ application/views/templates/footer.php
@@ -0,0 +1,42 @@
+<footer class="page-footer elegant-color">
+    <div class="container">
+        <div class="row">
+            <div class="col l6 s12">
+                <h5 class="white-text">Over ons</h5>
+                <p class="grey-text text-lighten-4">Overbodige maar nog bruikbare spulletjes een tweede leven geven, dat is het opzet van onze weggeefwinkel Het Gouden Hart. Zo verminderen we de afvalberg en maken we tegelijk heel wat mensen blij. Want wat de ene niet meer hoeft, kan de andere nog best gebruiken.</p>
+                <p>Keuzeproject 1ITF @ <a href="http://www.thomasmore.be/">Thomas More</a> prototype ontwikkeld door: <a href="#">Double Dev</a></p>
+
+            </div>
+            <div class="col l4 s12">
+                <h5 class="white-text">Contact</h5>
+                <ul id="dev_links">
+                    <li><i class="material-icons tiny">location_on</i> Locatie: Martinus&nbsp;Van&nbsp;Gurplaan&nbsp;41A 3920&nbsp;Lommel</li>
+                    <li><i class="material-icons tiny">phone</i> Tel.: 0494/94.67.77 (Annemarie&nbsp;Kota)</li>
+                    <li><i class="material-icons tiny">alternate_email</i> E-mail: vzwhetgoudenhart@gmail.com</li>
+                    <li><i class="material-icons tiny">home</i> Ondernemingsnummer: ON 0553687579</li>
+                </ul>
+            </div>
+            <div class="col l2 s12">
+                <h5 class="white-text">Social</h5>
+                <ul>
+                    <li>
+                        <a class="white-text" href="https://www.facebook.com/hetgoudenhart.vzw" target="_blank"><img src="afbeeldingen/facebook.png" alt="facebook pagina" width="50"></a>
+                        <a class="white-text" href="https://www.instagram.com/weggeefwinkelhetgoudenhart/" target="_blank"><img src="afbeeldingen/instagram.png" alt="instagram pagina" width="50"></a>
+                        <a class="white-text" href="https://www.youtube.com/watch?v=GJweeDW3MAI" target="_blank"><img src="afbeeldingen/youtube.png" alt="youtube pagina" width="50"></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-copyright">
+        <div class="container">
+            <i class="material-icons tiny">card_giftcard</i> Rekeningnummer: BE75&nbsp;0017&nbsp;2015&nbsp;4651
+        </div>
+    </div>
+</footer>
+<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script src="../../../js/materialize.js"></script>
+<script src="../../../js/init.js"></script>
+</body>
+
+</html>
---
+++ application/views/templates/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="nl">
+
+<head>
+    <meta charset="UTF-8">
+    <link href="../../../css/materialize.css" rel="stylesheet" type="text/css"/>
+    <link href="../../../css/main.css" rel="stylesheet" type="text/css"/>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
+    <title>
+        <?php echo $title; ?> | BelgianCarClub</title>
+</head>
+
+<body>
+    <div class="navbar-fixed">
+        <nav class="white">
+            <div class="nav-wrapper container">
+               <a id="logo-container" href="index.html" class="brand-logo center"><img src="../../../images/logo.png" alt="Logo van BelgianCarClub" style="width: 150px;"></a>
+                <ul class="hide-on-med-and-down right">
+                    <li><a href="#!">Home</a></li>
+                    <li><a href="#!">News</a></li>
+                    <li><a href="#!">Contact</a></li>
+                </ul>
+                <ul id="nav-mobile" class="sidenav">
+                    <li><a href="#!">Home</a></li>
+                    <li><a href="#!">News</a></li>
+                    <li><a href="#!">Contact</a></li>
+                </ul>
+                <a href="#!" data-target="nav-mobile" id="open-sidenav" class="sidenav-trigger primary-color right"><i class="material-icons">menu</i></a>
+            </div>
+        </nav>
+    </div>
