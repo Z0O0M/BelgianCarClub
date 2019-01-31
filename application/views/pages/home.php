9edd2744406bf6602292b02d08b760e63370b6fc
Z0O0M <cody.volz@hotmail.com>
2019-01-31 02:25:19 +0100

    Latest instagram posts

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,5 +1,15 @@
 <div id="index-banner" class="parallax-container">
-    <div class="section no-pad-bot">
-    </div>
+    <div class="section no-pad-bot"></div>
     <div class="parallax"><img src="../../../images/auto.jpg" alt="First background picture" style="transform: translate3d(-50%, 298.536px, 0px); opacity: 1;"></div>
+</div>
+<div class="row">
+<?php
+    $url = 'https://api.instagram.com/v1/users/self/media/recent/?access_token=7387140897.1677ed0.dc7b60ff409d441a91f2b103755dd359&count=3';
+    $output = file_get_contents($url);
+    $manage = json_decode($output, true);
+    foreach ($manage['data'] as $value) {
+        echo '<div class="col s12 m4"><blockquote class="instagram-media" data-instgrm-captioned data-instgrm-permalink="'.$value['link'].'" data-instgrm-version="12" style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:540px; min-width:326px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);"></blockquote></div>';
+    }
+?>
+<script async src="//www.instagram.com/embed.js"></script>
 </div>
--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -3,7 +3,12 @@
         <div class="row">
             <div class="col l6 s12">
                 <h5 class="white-text">About us</h5>
-                <p class="grey-text text-lighten-4">We're a Belgian automotive page </p>
+                <p class="grey-text text-lighten-4">We're a Belgian automotive page</p>
+                <ul class="grey-text text-lighten-4">
+                    <li>Daily Uploads of Belgian Cars</li>
+                    <li>Promotion of Automotive Events</li>
+                    <li>Since April 2018</li>
+                </ul>
 
 
             </div>
@@ -11,7 +16,7 @@
                 <h5 class="white-text">Contact</h5>
                 <ul id="dev_links">
                     <li><i class="material-icons tiny">location_on</i> Location: <a class="white-text" href="https://www.google.be/maps/place/Belgium/">Belgium </a></li>
-                    <li><i class="material-icons tiny">alternate_email</i> E-mail: <a class="white-text" href="mailto:belgiancarclub@gmail.com">  Belgiancarclub@gmail.com </a> </li>
+                    <li><i class="material-icons tiny">alternate_email</i> E-mail: <a class="white-text" href="mailto:belgiancarclub@gmail.com"> Belgiancarclub@gmail.com </a> </li>
                 </ul>
             </div>
             <div class="col l2 s12">
@@ -20,7 +25,6 @@
                     <li>
                         <a class="white-text" href="https://www.facebook.com/belgiancarclub/" target="_blank"><img src="../../../images/facebook.png" alt="facebook pagina" width="50"></a>
                         <a class="white-text" href="https://www.instagram.com/belgiancarclub/" target="_blank"><img src="../../../images/instagram.png" alt="instagram pagina" width="50"></a>
-
                     </li>
                 </ul>
             </div>
